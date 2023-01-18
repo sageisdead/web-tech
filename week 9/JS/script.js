1b03f4338c336ef8e2051f09c1a554d4b3edddbe
Bhojraj Adhikari <victusin7@gmail.com>
2023-01-18 11:29:11 +0545

    added missing week 3 and week 9 javascript also

---
+++ week 9/JS/script.js
@@ -0,0 +1,23 @@
+// alert ("Hi I am alert");
+// console.log("Hi Iam Mr adhikari" );
+// console.log("Iam 20 yrs old");
+// console.log(document.getElementById("paragraph").innerHTML);
+// document.getElementById("paragraph").innerHTML="<h1>Mr.Adhikari</h1>"
+// var bhojrajad="Hi Iam bhojrajad";
+// document.getElementById("my_message").innerHTML="yahoo"+" "+bhojrajad+"!"
+
+
+// var age;
+// var first_name="bhojraj";
+// var last_name="adhikari";
+// var salary=50000.99;
+// var phone_number="98562312"
+// console.log(age);
+
+function sum(a,b){
+    var sum=a+b;
+    console.log(sum);
+}
+sum(10,20);
+sum(20,30);
+sum(30,40);
