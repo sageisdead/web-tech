b4965fcd967b9b29b408a2e25fdce326146e2e9b
Bhojraj Adhikari <victusin7@gmail.com>
2023-02-01 10:04:22 +0545

    week 10 assignments

---
+++ week 10/js/query.js
@@ -0,0 +1,4 @@
+var select=document.querySelectorAll("p")
+for(var i=0;i<select.length;i++){
+    select[i].style.backgroundColor="pink";
+}
---
+++ week 10/js/variable.js
@@ -0,0 +1,22 @@
+//differents ways of variables declaration
+var name="Bhojraj";
+name="Sage";
+let name1="Bimal";
+name1="Omen";
+const name2="Brim";
+// name2="Viper";
+name3="Reyna";
+console.log(name1);
+console.log(name2);
+console.log(name3);
+
+let number = prompt("Enter a number:");
+console.log(number);
+//document.getElementsByClassName("paragraph")[2].style.backgroundColor="yellow";
+
+
+document.getElementsByClassName("paragraph")[0].style.backgroundColor="skyblue";
+document.getElementsByClassName("paragraph")[1].style.backgroundColor="pink";
+document.getElementsByClassName("paragraph")[2].style.backgroundColor="yellow";
+document.getElementsByClassName("paragraph")[2].innerHTML="Hy this is valorant!!!"
+document.getElementsByTagName("h1")[0].innerHTML="DOM getElementsByTagName example";
